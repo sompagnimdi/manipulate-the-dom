2f13f2113aef5cef400082c67dac6f6d565d9fdc
Irwin Tsay <irwintsay@gmail.com>
2017-01-19 12:23:26 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# DOM Manipulation
+
+Can you really *manipulate* DOM though?
+
+![Dom Toretto](https://i.giphy.com/uAo1LNpBnhhIc.gif)
+
+Yea I didn't think so... Thankfully we're not talking about *that* Dom.
+
+Tonight we're going to be practicing DOM manipulation and event handling.
+
+## Homework Submission:
+
+Homework is due tonight by **11pm**. Follow the [Homework Guide](https://git.generalassemb.ly/nyc-wdi-fisher/student-resources/blob/master/homework-guide.md) for submission instructions.
---
+++ script.js
@@ -0,0 +1,40 @@
+var quotes = [
+  'I live my life a quarter mile at a time',
+  'I said a ten-second car, not a ten-minute car',
+  'You can have any brew you want... as long as it\'s a Corona.',
+  'You almost had me? You never had me - you never had your car!',
+  'I don\'t have friends. I have family.',
+  'It don\'t matter if you win by an inch or a mile. Winning\'s winning.'
+];
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.querySelector('#quote-of-the-day').innerText = quotes[Math.floor(Math.random() * quotes.length)];
+  // Do all of your work inside the document.addEventListener
+  // Solve each problem within a function
+  
+
+  // Part 1
+
+
+
+
+
+
+
+  // document.body.style.backgroundColor = 'blue';
+
+
+
+  // 2. Append a new <li> with the text 'Fix cars' to Dom's Favorite Things
+
+  // 3. Remove the last <li> from Dom's Favorite Things
+
+
+
+
+
+
+
+
+
+});
