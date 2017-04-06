8bacd37a353e986248858c477013f368280540fe
Jon-zachary <Jon-zachary@users.noreply.github.com>
2017-04-06 11:18:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var quotes = [
-  'I live my life a quarter mile at a time',
-  'I said a ten-second car, not a ten-minute car',
-  'You can have any brew you want... as long as it\'s a Corona.',
-  'You almost had me? You never had me - you never had your car!',
-  'I don\'t have friends. I have family.',
-  'It don\'t matter if you win by an inch or a mile. Winning\'s winning.'
+let quotes = [
+  `I live my life a quarter mile at a time`,
+  `I said a ten-second car, not a ten-minute car`,
+  `You can have any brew you want... as long as it's a Corona.`,
+  `You almost had me? You never had me - you never had your car!`,
+  `I don't have friends. I have family.`,
+  `It don't matter if you win by an inch or a mile. Winning's winning.`
 ];
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // Random quote of the day generator
-  var randomQuote = function() {
+  const randomQuote = function() {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
