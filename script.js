2774ba4f7f4097ced2d5a8ae01eea02acb3b3724
Aboubacar Zoungrana <aboubacarprofessional@yahoo.com>
2022-11-15 06:52:17 -0600

    javaScript fixed

--- script.js
+++ script.js
@@ -16,33 +16,68 @@ document.addEventListener("DOMContentLoaded", function(event) {
   
   // Do all of your work inside the document.addEventListener  
 
-  // Part 1
+  // Part 1 DOM's personal website title is a bit wordy. Write a JavaScript statement that selects the #main-titleID element. Remember there are a couple of ways to query id. Change the text of the title to something shorter.
+
+  document.querySelector("#main-title").innerHTML="Wakanda Forever"
 
 
-  // Part 2
+
+  // Part 2 Select the body and change the background-color to a new color of your choice.
+
+  document.querySelector("body").style.backgroundColor="olive"
 
 
-  // Part 3
+  // Part 3 Select DOM's Favorite Things list and remove the last list item.
+
+  const favs = document.querySelector("#favorite-things")
+favs.removeChild(favs.lastElementChild)
 
 
-  // Part 4
+  // Part 4 .  Select all .special-titleclass elements and change their font-sizeto 2rem. Remember you might have to iterate through the list of elements
+
+  document.querySelectorAll(".special-title").forEach((el)=>{
+    el.style.fontSize = "2rem"
+  })
 
 
-  // Part 5
+  // Part 5 . Turns out DOM never raced in Chicago. Access the Past Races list and remove Chicago.
 
 
-  // Part 6
+  const races = document.querySelector("#past-races")
+  races.removeChild(races.children[3])
+
+  // Part 6   Let's add to DOM's Past Races list. Create a new <li>element, change the new <li>text to the name of a city, and append it to the Past Races list.
+
+  const li = document.createElement("li")
+  const newCity = "Ouagadougou"
+  li.innerHTML = newCity
+  races.append(li)
 
 
   // Part 7
-
+  let div = document.createElement("div")
+  div.classList.add("blog-post","purple")
+  
+  div.innerHTML = `<h1>${newCity}</h1>
+  <p>I RACED PAUL WALKER AND WON AGAIN!</p>
+  </div>`
+  document.querySelector(".main").append(div)
+  
 
   // Part 8
 
-
+  document.querySelector("#quote-title").addEventListener("click",randomQuote)
   // Part 9
 
-
+  document.querySelectorAll(".blog-post").forEach((el)=>{
+    el.addEventListener("mouseout",(event)=>{
+      el.classList.toggle("purple")
+    })
+    el.addEventListener("mouseenter",(event)=>{
+      el.classList.toggle("red")
+    })
+  })
 
 
 });
+
